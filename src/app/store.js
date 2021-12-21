fc609fbf46f9d27dd4ae9f7b0180f7b0bdfd3abd
Theophilus Boakye Yiadom <theophilus.yiadom@amalitech.org>
2021-12-21 11:35:35 -0800

    added firebase firestore

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "../features/users/userSlice";
+import movieSliceReducer from "../features/movie/movieSlice";
 const store = configureStore({
 	reducer: {
 		user: userSliceReducer,
+		movie: movieSliceReducer,
 	},
 });
 
---
+++ src/components/Recommends.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectRecommend } from "../features/movie/movieSlice";
+const Recommends = () => {
+	const movies = useSelector(selectRecommend);
+	console.log(movies);
+	return (
+		<Container>
+			<h4> Recomended for You</h4>
+			<Content>
+				{movies &&
+					movies.map((movie, i) => (
+						<Wrap key={i}>
+							{movie.id}
+							<Link to={`/detail/${i}`}>
+								<img src={movie.cardImg} alt={movie.title} />
+							</Link>
+						</Wrap>
+					))}
+			</Content>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	padding: 0 0 26px;
+`;
+
+const Content = styled.div`
+	display: grid;
+	gap: 24px;
+	grid-template-columns: repeat(4, minmax(0, 1fr));
+
+	@media (max-width: 768px) {
+		grid-template-columns: repeat(2, minmax(0, 1fr));
+	}
+`;
+
+const Wrap = styled.div`
+	padding-top: 56.25%;
+	border-radius: 10px;
+	box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+		rgb(0 0 0 /73%) 0px 16px 10px -10px;
+	cursor: pointer;
+	overflow: hidden;
+	position: relative;
+	transition: all 250ms cubic-bezier(0.25, 0.46, 0.4, 0.94);
+	border: 3px solid rgba(249, 249, 249, 0.1);
+
+	img {
+		display: block;
+		inset: 0px;
+		height: 100%;
+		position: absolute;
+		opacity: 1;
+		transition: all 250ms ease-in;
+		width: 100%;
+		z-index: 1;
+		top: 0;
+	}
+
+	&:hover {
+		box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+			rgb(0 0 0 /73%) 0px 16px 10px -10px;
+		transform: scale(1.05);
+		border-color: rgb(249, 249, 249);
+	}
+`;
+
+export default Recommends;
